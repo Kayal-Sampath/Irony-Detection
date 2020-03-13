4357f6258e4e82ea39b6c36498cdea45a3d0da7b
Kayal-Sampath <61958429+Kayal-Sampath@users.noreply.github.com>
2020-03-13 13:59:49 +0530

    Update csv_to_text_conversion.py

--- csv_to_text_conversion.py
+++ csv_to_text_conversion.py
@@ -1,6 +1,6 @@
 import csv
 f=open('a.txt','w+')
-with open('/home/hpc/Kayal/tasks/IDAT/training.csv') as csvDataFile:
+with open('./training.csv') as csvDataFile:
     csvReader = csv.reader(csvDataFile)
     data = [row for row in csv.reader(csvDataFile)]
     for i in range(4040):
